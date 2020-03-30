aa356761a0c4d4925db596486681eb33e2a2ec77
Olaide Abdulazeez <dj3plles@gmail.com>
2020-03-30 11:49:34 +0100

    Added fillables to models

--- app/Booking.php
+++ app/Booking.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    //
+    protected $fillable = ['parks_gardens_id', 'service_id', 'amount_booked', 'booking_status', 'time_booked', 'duration_booked'];
 }
